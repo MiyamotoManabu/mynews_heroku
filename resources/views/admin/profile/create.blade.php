b2a2a0071d0ff357f590f1d370d9abfd5216d057
Miyamoto <coldrain300@gmail.com>
2020-06-25 08:05:22 +0000

    Laravel013 ニュース作成画面、プロフィール作成画面修正、ルーティング変更

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -16,6 +16,53 @@ titleとcontentを編集しましょう。（ヒント: resources/views/admin/ne
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィールの新規作成</h2>
+                <!--Laravel013課題4【応用】 resources/views/admin/profile/create.blade.php を編集して、
+                氏名(name)、性別(gender)、趣味(hobby)、自己紹介欄(introduction)を入力するフォームを作成してください。
+                また、formの送信先(<form action=”この部分”>)を、 Admin\ProfileController の create Action に指定してください。
+                (ヒント: resources/views/admin/news/create.blade.php)-->
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" class="form-control" name="gender" value="{{ old('title') }}" checked> 男性
+                            <input type="radio" class="form-control" name="gender" value="{{ old('title') }}"> 女性
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">画像</label>
+                        <div class="col-md-10">
+                            <input type="file" class="form-control-file" name="image">
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+Route::get('news/create', 'Admin\NewsController@add');
+Route::post('news/create', 'Admin\NewsController@create'); # 追記
+/*Laravel013課題3【応用】 routes/web.php を編集して、 admin/profile/create に postメソッドでアクセスしたら
+ProfileController の create Action に割り当てるように設定してください。*/
+Route::post('profile/create', 'Admin\ProfileController@create'); 
+/*Laravel013課題6【応用】 routes/web.php を編集して、 admin/profile/edit に postメソッドでアクセスしたら 
+ProfileController の update Action に割り当てるように設定してください。*/
+Route::post('profile/edit', 'Admin\ProfileController@update');
     /*課題４【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
     web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
     admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。*/
